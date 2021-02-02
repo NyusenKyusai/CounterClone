aae3f1a6641925dcd36eef0c77b4d962fa6f42b8
Jonah <18018535@uhi.ac.uk>
2021-02-02 17:50:51 +0000

    Cloned Counter Application Repository Branch

--- src/counterapplication/CounterApplication.java
+++ src/counterapplication/CounterApplication.java
@@ -10,16 +10,22 @@ public class CounterApplication {
         public static void main(String[] args) {
         int choice;
         Counter sheep = new Counter();
+        
         FancyCounter goats = new FancyCounter();
+        
         Scanner stdin = new Scanner(System.in);
 
         System.out.println("Welcome to the Tally Counter application");
+        
         System.out.println("1 = Increment, 2 = Decrement, 3 = Reset, 0 = Exit");
         
         do {
             System.out.println("Number of sheep = " + sheep);
+            
             System.out.println("Number of goats = " + goats);
+            
             System.out.print("Next action: ");
+            
             choice = stdin.nextInt();
             if (choice == 1) {
                 sheep.increment();
